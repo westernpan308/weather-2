bae64d3eaab71c81b4171860b4683a8d3bc4caa9
westernpan308 <131693051+westernpan308@users.noreply.github.com>
2023-05-09 20:11:05 +0000

    fixing the code not sure how i did not save it

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-def main(city):
+while True:
+  def main():
     import json, requests
-
-    base_URL = "http://api.openweathermap.org/data/2.5/weather"
+#lines 3-10 are call to the URL web-service    
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
     appid = "d4463de4cefb6d542ee2810c3b787989"
-
-    URL = f"{base_URL}?q={city}&units=imperial&appid={appid}"
-    #print(URL)
-    #print()  
-#lines 14-15 are the call to and response from the URL     
-    response = requests.get(URL)
+    city = input("\nWhat city would you like to check the weather for?\n please enter a city or zip code.")
+    
+    url = f"{base_url}?q={city}&units=imperial&appid={appid}"
+    #print(url)
+    print()  
+    
+    response = requests.get(url)
     unformated_data = response.json ()
-#lines 17-31 is the data requested from the URL, has if else built into for ERROR so it will loop around   
+  
     if response.status_code == 200:
       print()
     else:
-      print(f"\nError Please check Spelling or Zip Code\n")
-      return
-
+      print(f"Error Please check Spelling or Zip Code")
+      main()
+  
     #print(unformated_data)
     name = unformated_data["name"]
     print(f"For the city of: {name}")
@@ -26,9 +28,18 @@ def main(city):
     print(f"The forecast high is: {temp_max}")
     temp_min = unformated_data["main"]["temp_min"]
     print(f"The forecast low is: {temp_min}\n")
-while True:
-    city = input("What city would you like to check the weather for?\n please enter a city or zip code.")
-    main(city)
+    
     print("Would you like to check the weather for another city?\n Y for yes\n N for no")
     option = input("y/n > ")
     print()
+      
+    if option == "y" or option == "Y":
+      main() 
+      
+    if option == "n" or option == "N":
+      print("Thank you and have a great day please return for more weather updates")
+      #break
+    else:
+      print("Error invalid entry please check spelling or ZIP code ")
+  main()
+main()
